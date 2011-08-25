985b10ec66f2aa2092d559d7bc766696aff538e4
HGrzywacz <hgrzywacz@gmail.com>
2011-08-25 23:48:27 +0200

    Big progress.

--- reddit2instapaper_v0.2.py
+++ reddit2instapaper_v0.2.py
@@ -1,28 +1,109 @@
-#!/usr/bin/env python2.7
+#!/usr/bin/python -O
+#/usr/bin/env python -O 
+"""README GOES HERE
+
+Version: 0.2beta
+Licence: GPLv2
+
+Copyright 2011 HGrzywacz <hgrzywacz@gmail.com>
+
+"""
 
 import sys
 import os
 import argparse
 import datetime
 import time
+import urllib
+import urllib2
 
 
-def main(argv):
-    parser = argparse.ArgumentParser(description='My learning parser')
-    parser.add_argument('action', nargs='*', type=str, help='Select action')
+def get_something():
+    """ I'll think of this something later """
+    pass
+
+
+def authenticate():
+    pass
+
+
+funcs = {'get':get_something, 'auth':authenticate}
+choices = funcs.keys()
+
+def main():
+
+    
+    parser = argparse.ArgumentParser(description='Help text')
+ 
+
+    parser.add_argument('actions', nargs='*', type=str, choices=choices,
+            help='Select action.')
+
     parser.add_argument('-n','-num', dest='number', nargs=1, default=4,
-            type=int, help='Number of top articles to fetch')
-    parser.add_argument('-t','-time', dest='time', nargs=1, default='3d',
-            type=str, help='Specifies time range from which latest articles'
-            ' will be fetched.')
+                    type=int, help='Number of top articles to fetch')
+    
+    parser.add_argument('-t','-time', dest='time', nargs=1, default=['3d'],
+                    type=str, help='Specifies time range from which latest articles'
+                    ' will be fetched.')
 
-    args = parser.parse_args(argv)
-    print args.action
+    args = parser.parse_args(sys.argv[1:])
+
+
+    print args.actions
     print args.number
     print args.time
+
     time_limit = parse_time(args.time[0])
+
     print time_limit
     print str(datetime.datetime.fromtimestamp(time_limit))
+
+    parse_actions(args.actions)
+
+
+def parse_actions(actions):
+    for action in actions:
+        if not action in funcs.keys():
+            print action + " is not a valid action."
+            sys.exit()
+        funcs[action]()
+        
+
+class Instapaper(object):
+    """ Instapaper API """
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+        self.url_auth = "https://www.instapaper.com/api/authenticate"
+        self.url_add = "https://www.instapaper.com/api/add"
+
+
+
+    def auth(self):
+        """ Method just for checking authentification. """
+        params = urlib.urlencode({'username':self.user, 
+                'password':self.password})
+        headers = {"Content-type": "application/x-www-form-urlencoded",
+                    "Accept": "text/plain"}
+
+        connection = httplib.HTTPConnection("www.instapaper.com")
+        connection.request("POST", "/api/authenticate", params, headers)
+    
+        response = connection.getresponse()
+    
+        if response.status == 201:
+            pass
+        elif response.status == 403:
+            print("Instapaper: Invalid username or password")
+            sys.exit()
+        elif response.status == 500:
+            print("Instapaper: The service encountered"
+                    " an error. Please try again later.")
+            sys.exit()
+
+
+ 
 
 
 class TimeException(Exception):
@@ -47,7 +128,9 @@ def parse_time(args_time):
     rest = args_time[0:-1]
     if rest is '':
         rest = "1"
-    print letter + ' ' + rest
+
+    if __debug__:
+        print letter + ' ' + rest
 
     seconds = 1
     minutes = 60 * seconds  # OCD-related
@@ -87,5 +170,5 @@ def parse_time(args_time):
 
 
 if __name__ == '__main__':
-    main(sys.argv)
+    main()
     sys.exit()
